efb14a8e03f9f531692c5f890a812d547cbbca16
Armando <arfega.anisgaro@gmail.com>
2020-11-16 18:28:20 +0100

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HOMEComponent } from './components/home/home.component';
+import { ProtectedComponent } from './components/protected/protected.component';
+import { PricesComponent } from './components/prices/prices.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HOMEComponent,
+    ProtectedComponent,
+    PricesComponent
   ],
   imports: [
     BrowserModule,
